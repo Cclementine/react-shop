4d3f17dc9274572744a0503941e85ec844791452
Hayden-qw <lifesh92@gmail.com>
2020-11-21 13:25:09 +0900

    upload file name success

--- boilerplate-mern-stack-master/client/src/components/views/UploadProductPage/UploadProductPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react'
 import {Typography, Button, Form, Input} from 'antd'
+import FileUpload from '../../utils/FileUpload';
 
 const { TextArea } = Input;
+
+const Continents = [
+    { key: 1, value: "Africa" },
+    { key: 2, value: "Europe" },
+    { key: 3, value: "Asia" },
+    { key: 4, value: "North America" },
+    { key: 5, value: "South America" },
+    { key: 6, value: "Austrailia" },
+    { key: 6, value: "Antarctica" }
+]
+
+
 
 function UploadProductPage() {
 
@@ -23,6 +36,10 @@ function UploadProductPage() {
         setPrice(event.currentTarget.value)
     }
 
+    const continentChangeHandler = (event) => {
+        setContinent(event.currentTarget.value)
+    }
+
 
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
@@ -33,6 +50,9 @@ function UploadProductPage() {
             <Form>
             
                 {/*drop zone*/}
+                <FileUpload />
+
+
                 <br />
                 <br />
                 <label>이름</label>
@@ -47,8 +67,11 @@ function UploadProductPage() {
                 <Input type="number" onChange={priceChangeHandler} value={Price}/>
                 <br />
                 <br />
-                <select>
-                    <option></option>
+                <select onChange={continentChangeHandler}>
+
+                    { Continents.map(item => (
+                        <option key={item.key} value={Continent}> {item.value} </option>
+                    ))}
                 </select>
                 <br />
                 <br />
